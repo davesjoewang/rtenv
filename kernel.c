9a90a2c4759f7072a42b3c48f2e13b1d29114a4e
davesjoewang <davesjoewang@gmail.com>
2014-02-20 17:20:32 +0800

    fix bug of modified strncmp()

--- kernel.c
+++ kernel.c
@@ -20,7 +20,7 @@ int strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i=0;
 
-	while(*s1 && *s1 == *s2 && i<=n)
+	while(*s1 && *s1 == *s2 && i<n)
 		*s1++, *s2++, i++;
 
 	return (*s1-*s2);
